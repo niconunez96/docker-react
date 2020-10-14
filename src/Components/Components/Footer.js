308c03b929f0570ad694e2f3cff463d341fe68c7
niconunez96 <nicolas110996@gmail.com>
2020-10-13 22:01:09 -0300

    Add more information to footer

--- src/Components/Components/Footer.js
+++ src/Components/Components/Footer.js
@@ -7,8 +7,20 @@ const Footer = (props) => {
         <section className="contactUs">
             <h3 className="contactUs-title">Contactanos</h3>
             <div className="contactUs-media">
-                <i className="fa fa-whatsapp fa-3x" aria-hidden="true"></i>
-                <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+                <a href="https://wa.link/l5b15d" target="_blank">
+                    <i className="fa fa-whatsapp fa-3x" aria-hidden="true"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jorge-sainer-lizarazo-254a621aa/" target="_blank">
+                    <i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i>
+                </a>
+                <a href="mailto:sainerlizarazo@hotmail.com.ar" target="_blank">
+                    <i class="fa fa-envelope-o fa-3x" aria-hidden="true"></i>
+                </a>
+            </div>
+            <hr/>
+            <div className="address">
+                <i class="fa fa-map-marker fa-2x" aria-hidden="true"></i>
+                <p>Gral. Manuel Belgrano s/n Cruz de Piedra - Maipu</p>
             </div>
         </section>
     )
